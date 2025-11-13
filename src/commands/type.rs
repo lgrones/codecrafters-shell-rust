9586bf3c8e59f4bb04def92f0eeac55ca91ef921
Leah Grones <leah.grones@gmx.at>
2025-11-13 19:44:24 +0100

    commit

--- src/commands/type.rs
+++ src/commands/type.rs
@@ -1,4 +1,11 @@
-use std::{any::Any, error::Error};
+use std::{
+    any::Any,
+    env,
+    error::Error,
+    fs::{self},
+};
+
+use is_executable::IsExecutable;
 
 use crate::commands::{create_command, noop::Noop, Command, Factory};
 
@@ -20,10 +27,30 @@ impl Command for Type {
 
         if !command.as_any().is::<Noop>() {
             println!("{} is a shell builtin", self.name);
-        } else {
-            println!("{}: not found", self.name);
+            return Ok(());
         }
 
+        let path_command = env::var_os("PATH")
+            .into_iter() // Option<OsString> -> iterator with 0 or 1 element
+            .flat_map(|os| env::split_paths(&os).collect::<Vec<_>>())
+            .find_map(|dir| {
+                fs::read_dir(dir).ok().and_then(|entries| {
+                    entries
+                        .filter_map(Result::ok)
+                        .find(|file| {
+                            file.path().is_executable()
+                                && file.file_name().to_string_lossy() == self.name
+                        })
+                        .map(|file| file.path())
+                })
+            });
+
+        if let Some(path) = path_command {
+            println!("{} is {}", self.name, path.to_string_lossy());
+            return Ok(());
+        }
+
+        println!("{}: not found", self.name);
         Ok(())
     }
 
