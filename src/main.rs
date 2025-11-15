d9b1f4d8ad3c998d000abac42a7d96f7e985fd52
Leah Grones <leah.grones@gmx.at>
2025-11-16 00:04:08 +0100

    commit

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,29 @@
-use reedline::{
-    default_emacs_keybindings, ColumnarMenu, DefaultCompleter, DefaultPrompt, DefaultPromptSegment,
-    Emacs, KeyCode, KeyModifiers, MenuBuilder, Reedline, ReedlineEvent, ReedlineMenu, Signal,
+use std::{
+    error::Error,
+    io::{self, Write},
 };
-use std::error::Error;
 
 mod commands;
 mod helper;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let commands = vec![
-        "test".into(),
-        "hello world".into(),
-        "hello world reedline".into(),
-        "this is the reedline crate".into(),
-    ];
-
-    let completer = Box::new(DefaultCompleter::new(commands.clone()));
-
-    let completion_menu = Box::new(ColumnarMenu::default().with_name("completion_menu"));
-
-    let mut keybindings = default_emacs_keybindings();
-    keybindings.add_binding(
-        KeyModifiers::NONE,
-        KeyCode::Tab,
-        ReedlineEvent::UntilFound(vec![
-            ReedlineEvent::Menu("completion_menu".to_string()),
-            ReedlineEvent::MenuNext,
-        ]),
-    );
-
-    let edit_mode = Box::new(Emacs::new(keybindings));
-
-    let mut editor = Reedline::create()
-        .with_completer(completer)
-        .with_menu(ReedlineMenu::EngineCompleter(completion_menu))
-        .with_edit_mode(edit_mode);
-
-    let prompt = DefaultPrompt::new(
-        DefaultPromptSegment::Basic(String::from("$ ")),
-        DefaultPromptSegment::Empty,
-    );
+    let mut command = String::new();
 
     loop {
-        match editor.read_line(&prompt) {
-            Ok(Signal::Success(command)) => {
-                let output = commands::create_command(&command).execute();
+        print!("$ ");
+        io::stdout().flush()?;
 
-                if let Some(out) = output.stdout {
-                    println!("{}", out.trim_end_matches("\n"));
-                }
+        command.clear();
+        io::stdin().read_line(&mut command)?;
 
-                if let Some(out) = output.stderr {
-                    println!("{}", out.trim_end_matches("\n"));
-                }
-            }
-            Ok(Signal::CtrlD) | Ok(Signal::CtrlC) => {
-                break;
-            }
-            x => {
-                x?;
-            }
+        let output = commands::create_command(&command).execute();
+
+        if let Some(out) = output.stdout {
+            println!("{}", out.trim_end_matches("\n"));
+        }
+
+        if let Some(out) = output.stderr {
+            println!("{}", out.trim_end_matches("\n"));
         }
     }
-
-    Ok(())
 }
